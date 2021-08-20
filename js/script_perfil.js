002bf6c75d9ddbf344d047b18a3c04b13dc46a5a
portal-sel <89226629+portal-sel@users.noreply.github.com>
2021-08-20 01:26:51 -0500

    Add files via upload

---
+++ js/script_perfil.js
@@ -0,0 +1,14 @@
+function setup() {
+    
+    setName()
+
+    docSelector = select('#selectDoc')
+
+    for (let index = 1; index < 10; index++) {
+        docSelector.option(`Documento ${index}`)
+        
+    }
+
+    btn_subir = select('#btnSubir')
+    btn_subir.attribute('disabled','')
+}
