9ea977a430e130f2d3df7f3bcf47c785837b4eaf
portal-sel <89226629+portal-sel@users.noreply.github.com>
2021-08-20 18:55:30 -0500

    Update script_perfil.js

--- js/script_perfil.js
+++ js/script_perfil.js
@@ -2,13 +2,6 @@ function setup() {
     
     setName()
 
-    docSelector = select('#selectDoc')
-
-    for (let index = 1; index < 10; index++) {
-        docSelector.option(`Documento ${index}`)
-        
-    }
-
     btn_subir = select('#btnSubir')
     btn_subir.attribute('disabled','')
     document.getElementById('txt_name').innerHTML = `SUPPLIER NAME ${localStorage.getItem('nombre')}`
